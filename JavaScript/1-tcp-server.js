7337023cc1a4fd02f65cd49903f3bd2b10245fea
Timur Shemsedinov <timur.shemsedinov@gmail.com>
2021-11-13 18:01:09 +0200

    Fix code style

--- JavaScript/1-tcp-server.js
+++ JavaScript/1-tcp-server.js
@@ -2,11 +2,11 @@
 
 const net = require('net');
 
-net.createServer(socket => {
+net.createServer((socket) => {
   console.dir(socket.address());
   socket.setNoDelay(true);
   socket.write('💗');
-  socket.on('data', data => {
+  socket.on('data', (data) => {
     console.log('📨:', data);
   });
 }).listen(2000);
--- JavaScript/6-udp-client.js
+++ JavaScript/6-udp-client.js
@@ -5,7 +5,7 @@ const dgram = require('dgram');
 const message = Buffer.from('Hello');
 const client = dgram.createSocket('udp4');
 
-client.send(message, 3000, 'localhost', err => {
+client.send(message, 3000, 'localhost', (err) => {
   if (err) {
     client.close();
     throw err;
